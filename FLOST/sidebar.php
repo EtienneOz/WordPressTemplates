c0bd9ba02aa78ba8a0d31bff72742a542f43958d
colm <mail@colm.be>
2014-08-07 18:22:27 +0200

    last mods to the sidebar to make it neutral - keeps the nice original functions proposing RSS, a search-bar + outputting the title and description of the blog in column

--- FLOST/sidebar.php
+++ FLOST/sidebar.php
@@ -27,40 +27,7 @@
               );?>
      </nav>
      
-     <p class="titrecategorie">
-        Ressources
-    </p>
-    <p class="souscategories">
-         <?php 
-         $args = array(
-            'show_option_all'    => '',
-            'orderby'            => 'name',
-            'order'              => 'ASC',
-            'style'              => 'none',
-            'show_count'         => 0,
-            'hide_empty'         => 1,
-            'use_desc_for_title' => 1,
-            'child_of'           => 0,
-            'feed'               => '',
-            'feed_type'          => '',
-            'feed_image'         => '',
-            'exclude'            => '',
-            'exclude_tree'       => '',
-            'include'            => '',
-            'hierarchical'       => 1,
-            'title_li'           => __( 'Categories' ),
-            'show_option_none'   => __('No categories'),
-            'number'             => null,
-            'echo'               => 1,
-            'depth'              => 0,
-            'current_category'   => 0,
-            'pad_counts'         => 0,
-            'taxonomy'           => 'category',
-            'walker'             => null
-        );
-        wp_list_categories( $args );
-        ?> 
-    </p>
+
   
   <!-- Formulaire de recherche -->
   <?php get_search_form(); ?>
